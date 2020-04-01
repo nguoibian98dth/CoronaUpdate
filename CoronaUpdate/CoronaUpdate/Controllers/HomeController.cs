71816ae437e7793542a19c236110cab368ab002b
nguoibian98dth <54108677+nguoibian98dth@users.noreply.github.com>
2020-04-01 22:11:52 +0700

    Init Update Corona Page

---
+++ CoronaUpdate/CoronaUpdate/Controllers/HomeController.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using CoronaUpdate.Models;
+
+namespace CoronaUpdate.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+
+        public IActionResult Corona()
+        {
+            return View();
+        }
+        public IActionResult Timeline()
+        {
+            return View();
+        }   
+    }
+}
